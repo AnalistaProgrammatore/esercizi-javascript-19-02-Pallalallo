6d68b838d773d1b4d7f9348d01ff43e9b414f5cd
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-02-25 13:56:04 +0100

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,8 @@
 /**
-* Scrivere qui il codice della funzione fibonacci(n)
+function FibonacciRic(x) {
+    if (x < 0) return false
+    if (x === 0 || x === 1) return 1
+    else return FibonacciRic(x - 1) + FibonacciRic(x - 2)
+}
+console.log(FibonacciRic(5))
 **/
