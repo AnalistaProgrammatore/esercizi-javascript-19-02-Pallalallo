f15c4536f73aa1ddbb20c87f531d354239df0d4f
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-02-27 23:01:35 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,39 @@
 /**
-* Scrivere qui il codice dell'esercizio 3 del capitolo 4 di eloquentJS
+function arrayToList(array) {
+    let aList = null;
+    for (let i = array.length - 1; i >= 0; i--) {
+        aList = { value: array[i], rest: aList }
+    }
+    return aList
+}
+function listToArray(list) {
+    let a = [];
+    i = 0;
+    while (list != null) {
+        a[i] = list.value;
+        i++;
+        list = list.rest;
+    }
+    return a;
+}
+function prepend(list, FirstEl) {
+    let newList = { value: FirstEl, rest: list }
+    return newList
+}
+
+function nth(list, Elem) {
+    let i = 0;
+    while (list.rest != null) {
+        if (Elem === i) return list.value
+        i++
+        list = list.rest;
+    }
+    if (!list) return undefined
+}
+
+function nthRic(list, Elem) {
+    if (!list) return undefined
+    else if (Elem == 0) return list.value
+    else return nthRic(list.rest, Elem - 1)
+}
 **/
