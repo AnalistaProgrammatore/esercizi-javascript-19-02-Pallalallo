aafd4e168e54f9be3f4f05c930340d5cfef6778c
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-02-27 23:05:10 +0100

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,4 +1,21 @@
 /**
-* Scrivere qui il codice dell'esercizio 2 del capitolo 4 di eloquentJS
-* Vi ricordo che le funzioni DEVONO ESSERE PURE e implementate una in modo IMPERATIVO e una in modo DICHIARATIVO
+let a = [1, 2, 3, 4, 5];
+function reverseArrayInPlace(a) {
+    for (i = 0; i <= (a.length - 1) / 2; i++) {
+        s = a[i];
+        a[i] = a[a.length - i - 1];
+        a[a.length - i - 1] = s;
+    }
+    return a;
+}
+function reverseArray(a) {
+    let b = [];
+    for (let i in a) {
+        b[i] = a[a.length - i - 1]
+    }
+    return b;
+}
+
+console.log(reverseArray(a))
+console.log(reverseArrayInPlace(a))
 **/
