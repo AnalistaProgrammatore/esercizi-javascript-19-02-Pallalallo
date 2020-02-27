ae2f1c290eee419b7dd6545f3d3fd09e7955ea94
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-02-27 23:02:55 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,40 @@
 /**
-* Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
+function stepRange(n1, n2, s) {
+    let a = [];
+    if (s === 0) {
+        return a.concat(n1, n2)
+    }
+    if (s > 0) {
+        for (i = n1; i <= n2; i += s) {
+            a = a.concat(i)
+        }
+    }
+    else {
+        for (i = n1; i >= n2; i += s) {
+            console.log('ciao')
+            a = a.concat(i)
+        }
+    }
+    return a;
+}
+
+
+function range(n1, n2) {
+    let a = [], m = n1;
+    for (i = 0; i <= n2 - n1; i++) {
+        a[i] = m;
+        m++
+    }
+    return a;
+}
+
+function sum(a) {
+    let current = 0;
+    for (let add of a) {
+        current += add;
+    }
+    return current;
+}
+console.log(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(stepRange(75, 2, 0))
 **/
