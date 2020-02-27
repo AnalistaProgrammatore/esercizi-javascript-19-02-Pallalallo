b694a5f1db2bfb4d2f3521e27634ab758ec44a50
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-02-27 23:02:19 +0100

    Update mcd.js

--- mcd.js
+++ mcd.js
@@ -1,3 +1,27 @@
 /**
-* Scrivere qui il codice delle funzioni mcd(x, y) e mcdRecursive(x, y)
+let x = 13, y = 0;
+function mcdRic(x, y) {
+    if (y === 0) return x
+    if ((x % y) === 0) {
+        return y;
+    }
+    else {
+        console.log('si')
+        return (mcdRic(y, (x % y)))
+    }
+}
+
+function mcd(x, y) {
+    if (y === 0) return x
+    while (x % y != 0) {
+        c = x;
+        x = y;
+        y = c % y;
+    }
+    return y;
+}
+
+console.log(mcd(x, y));
+console.log(mcdRic(x, y));
+
 **/
